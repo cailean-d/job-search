db7717df8b51805b7f960f495851144ef008f9ff
Denis Lykov <cailean393@gmail.com>
2018-02-15 00:38:11 +0300

    complete 'add' method(VacancyAddedResumeApi)

--- app/api/VacancyAddedResumeApi.php
+++ app/api/VacancyAddedResumeApi.php
@@ -1,7 +1,7 @@
 <?php
 
     /**
-     * @apiDefine auth Необходима авторизация
+     * @apiDefine auth Вы должны быть авторизированы под учетноый записью пользователя
      * 
      */
 
@@ -26,24 +26,66 @@
 
         }
 
+        /**
+         * 
+         * @api {post} vacancy_resume/:id Добавить свое резюме к вакансии
+         * @apiName AddVacancyResume
+         * @apiGroup VacancyResume
+         * @apiVersion  1.0.0
+         * 
+         * @apiPermission auth
+         * 
+         * @apiSuccess (200) {String} id ID пользователя
+         * @apiSuccess (200) {String} vacancy_id ID вакансии
+         * @apiSuccess (200) {String} user_id ID пользователя
+         * 
+         * @apiSuccessExample {json} Success-Response:
+         *  {
+         *      "id" : "1"
+         *      "vacancy_id" : "4"
+         *      "user_id" : "7"
+         *  }
+         * 
+         * @apiError RecordExists Вы уже отправили резюме
+         * @apiError VacancyDoesNotExist Вакансия не существует
+         * @apiError Auth Вы не авторизированы
+         * @apiError UserAuth Вы должны быть авторизированы под учетноый записью пользователя
+         *
+         * @apiErrorExample {json} Error-Response:
+         * 
+         *     HTTP/1.1 400 Bad Request
+         *     {
+         *       "error": "Вы уже отправили резюме"
+         *     }
+         * 
+         */
 
         public static function add($router){
             
             if(!isset($_SESSION['id'])){
 
-                Http::error('Вы не авторизированы');
+                Http::error('Вы не авторизированы', 403);
 
             }
 
             if($_SESSION['type'] != '0') { 
 
-                Http::error('Вы должны быть авторизированы под учетноый записью пользователя');
+                Http::error('Вы должны быть авторизированы под учетноый записью пользователя', 403);
 
             } 
 
+
+            $vacancy = Vacancy::get($router['id']);
+
+            if(empty($vacancy->getId())){
+
+                Http::error('Вакансия не существует');
+
+            }
+
             $addedResume = VacancyAddedResume::getByVacancyAndUserId($_SESSION['id'], $router['id']);
 
-            if(!empty($addedResume->geId())){
+            if(!empty($addedResume->getId())){
 
                 Http::error('Резюме уже отправлено');
 
@@ -51,8 +93,33 @@
 
             $newAddedResume = new VacancyAddedResume(null, $router['id'], $_SESSION['id']);
 
+            try{
+                
+                $newAddedResume->save();
 
+            } catch(Exception $e){
+                
+                if($e->getMessage() == 'vacancyId'){
 
+                    Http::error("Некорректное поле [vacancyId]");
+
+                } else if ($e->getMessage() == 'userid') {
+
+                    Http::error("Некорректное поле [userid]");
+
+                } 
+
+            }
+            
+            $res = [
+
+                'id' => $newAddedResume->getId(),
+                'vacancy_id' => $newAddedResume->getVacancyId(),
+                'user_id' => $newAddedResume->getUserId()
+
+            ];
+
+            Http::response($res, 200);
 
         }
 
