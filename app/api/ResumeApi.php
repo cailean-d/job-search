a7235d777304d18a3913024f6b9817f85ce6a03d
Denis Lykov <cailean393@gmail.com>
2018-02-18 01:31:41 +0300

    add 'myFullResume' method (ResumeApi)

--- app/api/ResumeApi.php
+++ app/api/ResumeApi.php
@@ -44,6 +44,12 @@
                     'method' => 'get',
                     'url' => 'resume/:id{number}/full',
                     'handler' => 'getFull'
+                ],
+
+                [
+                    'method' => 'get',
+                    'url' => 'resume/full',
+                    'handler' => 'myFullResume'
                 ]
 
             );
@@ -109,7 +115,7 @@
          *      "compskill_id" : "1",
          *      "car" : "Нет",
          *      "courses" : "Мои курсы...",
-         *      "skills" : "Мои навыки...",\
+         *      "skills" : "Мои навыки...",
          *  }
          * 
          * @apiError RecordNotFound Запись не найдена
@@ -212,10 +218,9 @@
          *      "compskill_id" : "1",
          *      "car" : "Нет",
          *      "courses" : "Мои курсы...",
-         *      "skills" : "Мои навыки...",\
+         *      "skills" : "Мои навыки...",
          *  }
          * 
-         * @apiError RecordNotFound Запись не найдена
          * @apiError Auth Вы не авторизированы
          * @apiError UserAuth Вы должны быть авторизированы под учетной записью пользователя
          *
@@ -282,8 +287,8 @@
 
         /**
          * 
-         * @api {get} resume/:id/full Полная запись
-         * @apiName GetFullResume
+         * @api {get} resume/full Полное резюме
+         * @apiName MyFullResume
          * @apiGroup Resume
          * @apiVersion  1.0.0
          * 
@@ -327,7 +332,55 @@
          *      "compskill" : "Уверенный пользователь",
          *      "car" : "Нет",
          *      "courses" : "Мои курсы...",
-         *      "skills" : "Мои навыки...",\
+         *      "skills" : "Мои навыки...",
+         *      "education": [
+         *           {
+         *               "level": "Среднее",
+         *               "inst": "ТГУ",
+         *               "city": "Москва",
+         *               "faculty": "",
+         *               "period": "2011 - 2012"
+         *           },
+         *           {
+         *               "level": "Неоконченное высшее",
+         *               "inst": "МГУ",
+         *               "city": "Москва",
+         *               "faculty": "Физико-математический",
+         *               "period": "2005 - 2010"
+         *           }
+         *       ],
+         *       "experience": [
+         *           {
+         *               "post": "Охранник",
+         *               "company": "Моя компания",
+         *               "city": "Город",
+         *               "industry": "Охрана, безопасность",
+         *               "period": "Июнь 2015 - Август 2015",
+         *               "functions": "Мои функции"
+         *           },
+         *           {
+         *               "post": "Сторож2",
+         *               "company": "Компания",
+         *               "city": "Город",
+         *               "industry": "Финансы, бухгалтерия",
+         *               "period": "Сентябрь 2010 - Октябрь 2015",
+         *               "functions": "Мои функции"
+         *           },
+         *           {
+         *               "post": "Сторож2",
+         *               "company": "Компания",
+         *               "city": "Город",
+         *               "industry": "Финансы, бухгалтерия",
+         *               "period": "Сентябрь 2010 - Октябрь 2015",
+         *               "functions": "Мои функции"
+         *           }
+         *       ],
+         *       "language": [
+         *           {
+         *               "language": "Украинский",
+         *               "lang_level": "Разговорный"
+         *           }
+         *       ]
          *  }
          * 
          * @apiError RecordNotFound Запись не найдена
@@ -382,6 +435,174 @@
 
             }
             
+        }
+
+        /**
+         * 
+         * @api {get} resume/:id/full Полная запись
+         * @apiName GetFullResume
+         * @apiGroup Resume
+         * @apiVersion  1.0.0
+         * 
+         * @apiPermission auth
+         * 
+         * @apiSuccess (200) {String} id ID записи
+         * @apiSuccess (200) {String} user_id ID пользователя
+         * @apiSuccess (200) {String} firstname Имя
+         * @apiSuccess (200) {String} lastname Фамилия
+         * @apiSuccess (200) {String} patronymic Отчество
+         * @apiSuccess (200) {String} gender Пол
+         * @apiSuccess (200) {String} birthday Дата рождения
+         * @apiSuccess (200) {String} city Город
+         * @apiSuccess (200) {String} phone Телефон
+         * @apiSuccess (200) {String} email Почта
+         * @apiSuccess (200) {String} post Желаемая должность
+         * @apiSuccess (200) {String} industry Отрасль
+         * @apiSuccess (200) {String} schedule Желаемый график работы
+         * @apiSuccess (200) {String} salary Желаемая заплата
+         * @apiSuccess (200) {String} workplace Желаемое место работы
+         * @apiSuccess (200) {String} compskill Владение компьютером
+         * @apiSuccess (200) {String} car Наличие авто
+         * @apiSuccess (200) {String} courses Пройденные курсы, тренинги
+         * @apiSuccess (200) {String} skills Навыки и умения
+         * 
+         * @apiSuccessExample {json} Success-Response:
+         *  {
+         *      "id" : "1",
+         *      "user_id" : "5",
+         *      "firstname" : "Петр",
+         *      "lastname" : "Петров",
+         *      "patronymic" : "Владимирович",
+         *      "gender" : "Мужской",
+         *      "birthday" : "01.01.1980",
+         *      "city" : "Москва",
+         *      "phone" : "+79999999999",
+         *      "email" : "test@test.com",
+         *      "post" : "Сторож",
+         *      "industry" : "Охрана, безопасность",
+         *      "schedule" : "Вахтовый",
+         *      "salary" : "99999",
+         *      "workplace" : "На территории работодателя",
+         *      "compskill" : "Уверенный пользователь",
+         *      "car" : "Нет",
+         *      "courses" : "Мои курсы...",
+         *      "skills" : "Мои навыки...",
+         *      "education": [
+         *           {
+         *               "level": "Среднее",
+         *               "inst": "ТГУ",
+         *               "city": "Москва",
+         *               "faculty": "",
+         *               "period": "2011 - 2012"
+         *           },
+         *           {
+         *               "level": "Неоконченное высшее",
+         *               "inst": "МГУ",
+         *               "city": "Москва",
+         *               "faculty": "Физико-математический",
+         *               "period": "2005 - 2010"
+         *           }
+         *       ],
+         *       "experience": [
+         *           {
+         *               "post": "Охранник",
+         *               "company": "Моя компания",
+         *               "city": "Город",
+         *               "industry": "Охрана, безопасность",
+         *               "period": "Июнь 2015 - Август 2015",
+         *               "functions": "Мои функции"
+         *           },
+         *           {
+         *               "post": "Сторож2",
+         *               "company": "Компания",
+         *               "city": "Город",
+         *               "industry": "Финансы, бухгалтерия",
+         *               "period": "Сентябрь 2010 - Октябрь 2015",
+         *               "functions": "Мои функции"
+         *           },
+         *           {
+         *               "post": "Сторож2",
+         *               "company": "Компания",
+         *               "city": "Город",
+         *               "industry": "Финансы, бухгалтерия",
+         *               "period": "Сентябрь 2010 - Октябрь 2015",
+         *               "functions": "Мои функции"
+         *           }
+         *       ],
+         *       "language": [
+         *           {
+         *               "language": "Украинский",
+         *               "lang_level": "Разговорный"
+         *           }
+         *       ]
+         *  }
+         * 
+         * @apiError Auth Вы не авторизированы
+         * @apiError UserAuth Вы должны быть авторизированы под учетной записью пользователя
+         *
+         * @apiErrorExample {json} Error-Response:
+         * 
+         *     HTTP/1.1 400 Bad Request
+         *     {
+         *       "error": "Запись не найдена"
+         *     }
+         * 
+         */
+
+        public static function myFullResume(){
+                                 
+            if(!isset($_SESSION['id'])){
+
+                Http::error('Вы не авторизированы');
+
+            }
+            
+            if($_SESSION['type'] != '0') { 
+
+                Http::error('Вы должны быть авторизированы под учетной записью пользователя', 403);
+
+            } 
+
+            
+            $resume = Resume::getJoinedByUserId($_SESSION['id']);
+
+            if (empty($resume->getId())) {
+
+                Http::error('Запись не найдена');
+
+            } else {
+
+                $res = [
+
+                    'id' => $resume->getId(),
+                    'user_id' => $resume->getUserid(),
+                    'firstname' => $resume->getFirstname(),
+                    'lastname' => $resume->getLastname(),
+                    'patronymic' => $resume->getPatronymic(),
+                    'gender' => $resume->getGender(),
+                    'birthday' => $resume->getBirthday(),
+                    'city' => $resume->getCity(),
+                    'phone' => $resume->getPhone(),
+                    'email' => $resume->getEmail(),
+                    'post' => $resume->getPost(),
+                    'industry' => $resume->getIndustryName(),
+                    'schedule' => $resume->getScheduleName(),
+                    'salary' => $resume->getSalary(),
+                    'workplace' => $resume->getWorkPlaceName(),
+                    'compskill' => $resume->getCompSkillName(),
+                    'car' => $resume->getCar(),
+                    'courses' => $resume->getCourses(),
+                    'skills' => $resume->getSkills(),
+                    'education' => self::getEducation(),
+                    'experience' => self::getExperience(),
+                    'language' => self::getLanguage()
+
+                ];
+
+                Http::response($res, 200);
+
+            }
+
         }
 
         private function getEducation(){
