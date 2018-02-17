67ec1d7869cc0917b31d59fb901897d6c9ac4c48
Denis Lykov <cailean393@gmail.com>
2018-02-18 00:57:27 +0300

    add 'myResume' method (ResumeApi)

--- app/api/ResumeApi.php
+++ app/api/ResumeApi.php
@@ -32,6 +32,12 @@
                     'method' => 'get',
                     'url' => 'resume/:id{number}',
                     'handler' => 'get'
+                ],
+
+                [
+                    'method' => 'get',
+                    'url' => 'resume',
+                    'handler' => 'myResume'
                 ]
 
             );
@@ -149,6 +155,123 @@
 
             }
             
+        }
+
+        /**
+         * 
+         * @api {get} resume Мое резюме
+         * @apiName GetMyResume
+         * @apiGroup Resume
+         * @apiVersion  1.0.0
+         * 
+         * @apiPermission auth
+         * 
+         * @apiSuccess (200) {String} id ID записи
+         * @apiSuccess (200) {String} user_id ID пользователя
+         * @apiSuccess (200) {String} firstname Имя
+         * @apiSuccess (200) {String} lastname Фамилия
+         * @apiSuccess (200) {String} patronymic Отчество
+         * @apiSuccess (200) {String} gender Пол
+         * @apiSuccess (200) {String} birthday Дата рождения
+         * @apiSuccess (200) {String} city Город
+         * @apiSuccess (200) {String} phone Телефон
+         * @apiSuccess (200) {String} email Почта
+         * @apiSuccess (200) {String} post Желаемая должность
+         * @apiSuccess (200) {String} industry_id ID отрасли
+         * @apiSuccess (200) {String} schedule_id Желаемый график работы
+         * @apiSuccess (200) {String} salary Желаемая заплата
+         * @apiSuccess (200) {String} workplace_id Желаемое место работы
+         * @apiSuccess (200) {String} compskill_id Владение компьютером
+         * @apiSuccess (200) {String} car Наличие авто
+         * @apiSuccess (200) {String} courses Пройденные курсы, тренинги
+         * @apiSuccess (200) {String} skills Навыки и умения
+         * 
+         * @apiSuccessExample {json} Success-Response:
+         *  {
+         *      "id" : "1",
+         *      "user_id" : "5",
+         *      "firstname" : "Петр",
+         *      "lastname" : "Петров",
+         *      "patronymic" : "Владимирович",
+         *      "gender" : "Мужской",
+         *      "birthday" : "01.01.1980",
+         *      "city" : "Москва",
+         *      "phone" : "+79999999999",
+         *      "email" : "test@test.com",
+         *      "post" : "Сторож",
+         *      "industry_id" : "5",
+         *      "schedule_id" : "3",
+         *      "salary" : "99999",
+         *      "workplace_id" : "2",
+         *      "compskill_id" : "1",
+         *      "car" : "Нет",
+         *      "courses" : "Мои курсы...",
+         *      "skills" : "Мои навыки...",\
+         *  }
+         * 
+         * @apiError RecordNotFound Запись не найдена
+         * @apiError Auth Вы не авторизированы
+         * @apiError UserAuth Вы должны быть авторизированы под учетной записью пользователя
+         *
+         * @apiErrorExample {json} Error-Response:
+         * 
+         *     HTTP/1.1 400 Bad Request
+         *     {
+         *       "error": "Вы должны быть авторизированы под учетной записью пользователя"
+         *     }
+         * 
+         */
+
+        public static function myResume(){
+                     
+            if(!isset($_SESSION['id'])){
+
+                Http::error('Вы не авторизированы');
+
+            }
+            
+            if($_SESSION['type'] != '0') { 
+
+                Http::error('Вы должны быть авторизированы под учетной записью пользователя', 403);
+
+            } 
+
+            $resume = Resume::getByUserId($_SESSION['id']);
+
+            if (empty($resume->getId())) {
+
+                Http::error('Запись не найдена');
+
+            } else {
+
+                $res = [
+
+                    'id' => $resume->getId(),
+                    'user_id' => $resume->getUserid(),
+                    'firstname' => $resume->getFirstname(),
+                    'lastname' => $resume->getLastname(),
+                    'patronymic' => $resume->getPatronymic(),
+                    'gender' => $resume->getGender(),
+                    'birthday' => $resume->getBirthday(),
+                    'city' => $resume->getCity(),
+                    'phone' => $resume->getPhone(),
+                    'email' => $resume->getEmail(),
+                    'post' => $resume->getPost(),
+                    'industry_id' => $resume->getIndustryId(),
+                    'schedule_id' => $resume->getScheduleId(),
+                    'salary' => $resume->getSalary(),
+                    'workplace_id' => $resume->getWorkPlaceId(),
+                    'compskill_id' => $resume->getCompSkillId(),
+                    'car' => $resume->getCar(),
+                    'courses' => $resume->getCourses(),
+                    'skills' => $resume->getSkills(),
+
+                ];
+
+                Http::response($res, 200);
+
+            }
+
         }
 
      }
