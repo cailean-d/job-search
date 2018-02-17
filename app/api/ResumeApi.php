1b83d97b26d71237135e48f186500db49d7c0a3e
Denis Lykov <cailean393@gmail.com>
2018-02-18 01:22:47 +0300

    add 'getFull' method (ResumeApi)

--- app/api/ResumeApi.php
+++ app/api/ResumeApi.php
@@ -38,6 +38,12 @@
                     'method' => 'get',
                     'url' => 'resume',
                     'handler' => 'myResume'
+                ],
+
+                [
+                    'method' => 'get',
+                    'url' => 'resume/:id{number}/full',
+                    'handler' => 'getFull'
                 ]
 
             );
@@ -271,6 +277,192 @@
                 Http::response($res, 200);
 
             }
+
+        }
+
+        /**
+         * 
+         * @api {get} resume/:id/full Полная запись
+         * @apiName GetFullResume
+         * @apiGroup Resume
+         * @apiVersion  1.0.0
+         * 
+         * @apiSuccess (200) {String} id ID записи
+         * @apiSuccess (200) {String} user_id ID пользователя
+         * @apiSuccess (200) {String} firstname Имя
+         * @apiSuccess (200) {String} lastname Фамилия
+         * @apiSuccess (200) {String} patronymic Отчество
+         * @apiSuccess (200) {String} gender Пол
+         * @apiSuccess (200) {String} birthday Дата рождения
+         * @apiSuccess (200) {String} city Город
+         * @apiSuccess (200) {String} phone Телефон
+         * @apiSuccess (200) {String} email Почта
+         * @apiSuccess (200) {String} post Желаемая должность
+         * @apiSuccess (200) {String} industry Отрасль
+         * @apiSuccess (200) {String} schedule Желаемый график работы
+         * @apiSuccess (200) {String} salary Желаемая заплата
+         * @apiSuccess (200) {String} workplace Желаемое место работы
+         * @apiSuccess (200) {String} compskill Владение компьютером
+         * @apiSuccess (200) {String} car Наличие авто
+         * @apiSuccess (200) {String} courses Пройденные курсы, тренинги
+         * @apiSuccess (200) {String} skills Навыки и умения
+         * 
+         * @apiSuccessExample {json} Success-Response:
+         *  {
+         *      "id" : "1",
+         *      "user_id" : "5",
+         *      "firstname" : "Петр",
+         *      "lastname" : "Петров",
+         *      "patronymic" : "Владимирович",
+         *      "gender" : "Мужской",
+         *      "birthday" : "01.01.1980",
+         *      "city" : "Москва",
+         *      "phone" : "+79999999999",
+         *      "email" : "test@test.com",
+         *      "post" : "Сторож",
+         *      "industry" : "Охрана, безопасность",
+         *      "schedule" : "Вахтовый",
+         *      "salary" : "99999",
+         *      "workplace" : "На территории работодателя",
+         *      "compskill" : "Уверенный пользователь",
+         *      "car" : "Нет",
+         *      "courses" : "Мои курсы...",
+         *      "skills" : "Мои навыки...",\
+         *  }
+         * 
+         * @apiError RecordNotFound Запись не найдена
+         *
+         * @apiErrorExample {json} Error-Response:
+         * 
+         *     HTTP/1.1 400 Bad Request
+         *     {
+         *       "error": "Запись не найдена"
+         *     }
+         * 
+         */
+
+        public static function getFull($router){
+
+            $resume = Resume::getJoinedByUserId($router['id']);
+
+            if (empty($resume->getId())) {
+
+                Http::error('Запись не найдена');
+
+            } else {
+
+                $res = [
+
+                    'id' => $resume->getId(),
+                    'user_id' => $resume->getUserid(),
+                    'firstname' => $resume->getFirstname(),
+                    'lastname' => $resume->getLastname(),
+                    'patronymic' => $resume->getPatronymic(),
+                    'gender' => $resume->getGender(),
+                    'birthday' => $resume->getBirthday(),
+                    'city' => $resume->getCity(),
+                    'phone' => $resume->getPhone(),
+                    'email' => $resume->getEmail(),
+                    'post' => $resume->getPost(),
+                    'industry' => $resume->getIndustryName(),
+                    'schedule' => $resume->getScheduleName(),
+                    'salary' => $resume->getSalary(),
+                    'workplace' => $resume->getWorkPlaceName(),
+                    'compskill' => $resume->getCompSkillName(),
+                    'car' => $resume->getCar(),
+                    'courses' => $resume->getCourses(),
+                    'skills' => $resume->getSkills(),
+                    'education' => self::getEducation(),
+                    'experience' => self::getExperience(),
+                    'language' => self::getLanguage()
+
+                ];
+
+                Http::response($res, 200);
+
+            }
+            
+        }
+
+        private function getEducation(){
+
+            $education = Education::getByUserId($_SESSION['id']);
+
+            $edu_arr = array();
+
+            foreach ($education as $edu) {
+
+                $help_edu = HelperEducation::get($edu->getLevelId());
+
+                array_push($edu_arr,
+
+                [
+
+                    'level' => $help_edu->getName(),
+                    'inst' => $edu->getInstitute(),
+                    'city' => $edu->getCity(),
+                    'faculty' => $edu->getFaculty(),
+                    'period' => $edu->getStudyPeriod()
+    
+                ]);
+
+            }
+
+            return $edu_arr;
+
+        }
+
+        private function getLanguage(){
+            
+            $language = Language::getByUserId($_SESSION['id']);
+
+            $res = array();
+
+            foreach ($language as $lang) {
+
+                $help_lang = HelperLanguage::get($lang->getLangId());
+
+                array_push($res,
+
+                [
+
+                    'language' => $help_lang->getName(),
+                    'lang_level' => $lang->getLangLevel()
+    
+                ]);
+
+            }
+
+            return $res;
+
+        }
+
+        private function getExperience(){
+
+            $experience = Experience::getByUserId($_SESSION['id']);
+
+            $res = array();
+
+            foreach ($experience as $exp) {
+
+                $help_exp = HelperIndustry::get($exp->getIndustryId());
+
+                array_push($res,
+
+                [
+
+                    'post' => $exp->getPost(),
+                    'company' => $exp->getCompany(),
+                    'city' => $exp->getCity(),
+                    'industry' => $help_exp->getName(),
+                    'period' => $exp->getWorkPeriod(),
+                    'functions' => $exp->getFunctions()
+    
+                ]);
+
+            }
+
+            return $res;
 
         }
 
